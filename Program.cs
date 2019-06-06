10678a60b1a361d77904f43826a57c46a3886c87
Amelie Certin <amelie.certin@gmail.com>
2019-06-06 10:13:18 +0200

    Init application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace poney_valley
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
